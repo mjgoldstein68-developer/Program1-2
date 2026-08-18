99829e7fd3b13b24cbb3c62da5835fde2427ab20
MarkVPCC <goldsteinm@vpcc.edu>
2026-08-18 10:02:11 -0400

    Add project files.

---
+++ Program1_2.cpp
@@ -0,0 +1,31 @@
+/*
+    File Name: Program1-2.cpp
+    Software Engineer: Lastname Firstname
+    StudentID:
+    Date:MM/YYYY
+    Requirements:
+
+
+    Design Outline:
+        1: Define Variables
+        2. Get Input or assign variables from Requirements
+        3. Process
+        4. Output Results
+
+*/
+
+#include <iostream>
+using namespace std;
+//using  std::cin, std::cout, std::endl;
+
+int main()
+{
+    
+
+    cout << "Hi! it\'s me.\n";
+    cout <<"I\'m learning to program!\n";
+     
+    return 0;
+
+}
+
